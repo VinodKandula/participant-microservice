1c356e5ee2e9f903fbbf8ab7d73b8f02383994a8
Mahan Hashemizadeh <mahanhz@gmail.com>
2016-05-30 00:43:40 +0200

    Initial commit

---
+++ src/main/java/com/amhzing/participant/ParticipantMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.amhzing.participant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParticipantMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParticipantMicroserviceApplication.class, args);
+	}
+}
