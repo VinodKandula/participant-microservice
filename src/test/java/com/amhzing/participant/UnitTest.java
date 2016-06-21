740a9e9ed9b54ce4d8ffa63bb48c4a1d9b58dd57
Mahan Hashemizadeh <mahan16@hotmail.com>
2016-06-21 16:29:09 +0200

    pass

--- src/test/java/com/amhzing/participant/UnitTest.java
+++ src/test/java/com/amhzing/participant/UnitTest.java
@@ -2,14 +2,11 @@ package com.amhzing.participant;
 
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
-
 public class UnitTest {
 
     //Pass
     @Test
     public void contextLoads() {
-        fail();
     }
 
 }
