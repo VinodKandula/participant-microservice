d4515fe8332dcf2f904ae925882e7b31f8e3f33b
Mahan Hashemizadeh <mahan16@hotmail.com>
2016-06-27 00:11:38 +0200

    Return participantId when creating

--- src/main/java/com/amhzing/participant/web/ParticipantController.java
+++ src/main/java/com/amhzing/participant/web/ParticipantController.java
@@ -1,11 +1,12 @@
 package com.amhzing.participant.web;
 
 import com.amhzing.participant.api.Name;
+import com.amhzing.participant.api.ParticipantId;
 import com.amhzing.participant.api.command.CreateParticipantCommand;
 import com.amhzing.participant.gateway.MetaDataEnrichedCommandGateway;
 import com.fasterxml.uuid.Generators;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,22 +17,21 @@ import java.util.UUID;
 import java.util.concurrent.TimeoutException;
 
 @RestController
-@RequestMapping(path = "/participant", consumes = "application/json")
+@RequestMapping(path = "/participant", consumes = MediaType.APPLICATION_JSON_VALUE)
 public class ParticipantController {
 
     @Autowired
     private MetaDataEnrichedCommandGateway commandGateway;
 
-    @RequestMapping(path = "create", method = RequestMethod.POST)
-    @Transactional
-    public String create(@RequestBody @Valid final Name name) throws TimeoutException, InterruptedException {
+    @RequestMapping(path = "create", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public @Valid ParticipantId create(@RequestBody @Valid final Name name) throws TimeoutException, InterruptedException {
 
         final UUID uuid = Generators.timeBasedGenerator().generate();
 
         final CreateParticipantCommand command = new CreateParticipantCommand(uuid, name);
-        commandGateway.sendAndWait(command, "56789", "mahnDT2");
+        commandGateway.send(command, "56789", "mahnDT2");
 
-        return name.toString();
+        return ParticipantId.create(command.getId().toString());
     }
 
 }
