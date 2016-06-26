93b66cfe55d02ae0f0e1a40ee41e21c1812559bc
Mahan Hashemizadeh <mahan16@hotmail.com>
2016-06-26 23:26:53 +0200

    Added validation to Name api

--- src/main/java/com/amhzing/participant/api/Name.java
+++ src/main/java/com/amhzing/participant/api/Name.java
@@ -1,13 +1,26 @@
 package com.amhzing.participant.api;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
+@JsonInclude
 public class Name {
+    private static final String LENGTH_HAS_BEEN_EXCEEDED = "Length has been exceeded";
 
+    @Size(message = LENGTH_HAS_BEEN_EXCEEDED, max = 25)
     private final String firstName;
+    @Size(message = LENGTH_HAS_BEEN_EXCEEDED, max = 25)
     private final String middleName;
+    @NotNull @Size(message = LENGTH_HAS_BEEN_EXCEEDED, min= 1, max = 25)
     private final String lastName;
+    @Size(message = LENGTH_HAS_BEEN_EXCEEDED, max = 10)
     private final String suffix;
+
 
     private Name(final String firstName, final String middleName, final String lastName, final String suffix) {
         this.firstName = firstName;
@@ -16,10 +29,11 @@ public class Name {
         this.suffix = suffix;
     }
 
-    public static Name create(final String firstName,
-                              final String middleName,
-                              final String lastName,
-                              final String suffix) {
+    @JsonCreator
+    public static Name create(@JsonProperty("firstName") final String firstName,
+                              @JsonProperty("middleName") final String middleName,
+                              @JsonProperty("lastName") final String lastName,
+                              @JsonProperty("suffix") final String suffix) {
         return new Name(firstName, middleName, lastName, suffix);
     }
 
