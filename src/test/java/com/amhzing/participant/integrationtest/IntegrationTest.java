27b3364467cffe5863e46bc304ef0e8f8f560e52
Mahan Hashemizadeh <mahanhz@gmail.com>
2016-06-28 00:24:07 +0200

    Added local and production profiles

--- src/test/java/com/amhzing/participant/integrationtest/IntegrationTest.java
+++ src/test/java/com/amhzing/participant/integrationtest/IntegrationTest.java
@@ -1,6 +1,7 @@
 package com.amhzing.participant.integrationtest;
 
 import com.amhzing.participant.ParticipantApplication;
+import com.amhzing.participant.annotation.IfProfileProduction;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
@@ -9,7 +10,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ParticipantApplication.class)
-@WebIntegrationTest
+@WebIntegrationTest({"server.port=0", "management.port=0"})
+@IfProfileProduction
 public class IntegrationTest {
 
     @Test
