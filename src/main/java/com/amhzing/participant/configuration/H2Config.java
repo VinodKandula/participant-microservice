53f27db947c85dff8a73ae1359c6e11203640ecd
Mahan Hashemizadeh <mahanhz@gmail.com>
2016-09-06 22:38:50 +0200

    Add H2Config

--- src/main/java/com/amhzing/participant/configuration/H2Config.java
+++ src/main/java/com/amhzing/participant/configuration/H2Config.java
@@ -1,4 +1,12 @@
 package com.amhzing.participant.configuration;
 
+import com.amhzing.participant.annotation.Offline;
+import com.amhzing.participant.query.data.jpa.repository.ParticipantQueryDslRepository;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Offline
+@Configuration
+@EnableJpaRepositories(basePackageClasses = {ParticipantQueryDslRepository.class})
 public class H2Config {
 }
