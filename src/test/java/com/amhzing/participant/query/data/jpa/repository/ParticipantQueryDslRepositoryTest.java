9cbbf7a1bc1594c0c44d8c168449ec67100e8986
Mahan Hashemizadeh <mahanhz@gmail.com>
2016-09-09 20:40:17 +0200

    Added and tidied up repository tests

--- src/test/java/com/amhzing/participant/query/data/jpa/repository/ParticipantQueryDslRepositoryTest.java
+++ src/test/java/com/amhzing/participant/query/data/jpa/repository/ParticipantQueryDslRepositoryTest.java
@@ -2,6 +2,7 @@ package com.amhzing.participant.query.data.jpa.repository;
 
 import com.amhzing.participant.query.data.jpa.mapping.ParticipantDetails;
 import com.fasterxml.uuid.Generators;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,18 +13,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.List;
 import java.util.UUID;
 
+import static com.amhzing.participant.query.data.jpa.mapping.QParticipantDetails.participantDetails;
 import static com.amhzing.participant.query.data.jpa.repository.ParticipantRepositoryHelper.*;
-import static com.amhzing.participant.query.data.jpa.repository.ParticipantRepositoryHelper.assertParticipantDetails;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-public class ParticipantRepositoryTest {
+public class ParticipantQueryDslRepositoryTest {
 
     @Autowired
     private TestEntityManager entityManager;
 
     @Autowired
-    private ParticipantRepository repository;
+    private ParticipantQueryDslRepository repository;
 
     private UUID participantId = Generators.timeBasedGenerator().generate();
 
@@ -31,7 +32,10 @@ public class ParticipantRepositoryTest {
     public void should_insert_participant() throws Exception {
         // This is a workaround as entityManager.persist expects @GeneratedValue on the @Id
         entityManager.getEntityManager().persist(participant(participantId));
-        final List<ParticipantDetails> participants = this.repository.findByAddress_CountryAllIgnoreCase("US");
+
+        final BooleanExpression predicate = participantDetails.address.country.equalsIgnoreCase("us");
+
+        final List<ParticipantDetails> participants = this.repository.findAll(predicate);
 
         assertParticipantDetails(participants);
     }
