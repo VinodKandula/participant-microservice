49efc177c215c5e87eff81b7385ef76212e45500
Mahan Hashemizadeh <mahanhz@gmail.com>
2016-09-26 20:41:04 +0200

    Add @Configuration to EurekaConfig

--- src/main/java/com/amhzing/participant/configuration/EurekaConfig.java
+++ src/main/java/com/amhzing/participant/configuration/EurekaConfig.java
@@ -2,8 +2,10 @@ package com.amhzing.participant.configuration;
 
 import com.amhzing.participant.annotation.Online;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
 
 @Online
+@Configuration
 @EnableEurekaClient
 public class EurekaConfig {
 }
